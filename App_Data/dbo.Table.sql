3ee32a3d7526510388f93014ea782bdfb4a7265d
Gale100 <avinashgale100@gmail.com>
2018-07-21 14:34:33 -0500

    Add files via upload

---
+++ App_Data/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[OrderId] INT NOT NULL PRIMARY KEY, 
+    [UserId] NVARCHAR(50) NOT NULL, 
+    [OrderType] NCHAR(10) NOT NULL, 
+    [DeliveryAddress] NCHAR(10) NOT NULL, 
+    [DeliveryDate] CHAR(10) NOT NULL, 
+    [PaymentType] NCHAR(10) NOT NULL
+)
